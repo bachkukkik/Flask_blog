bdf9c117637e3f44357befcfcb5f3014730d4b56
Krichkorn Oparad <krichkorn@gmail.com>
2019-12-31 18:53:22 +0700

    FlaskForms - 1st attempt
    Corey Schafer - Flask Tutorial EP3 - 22.17

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET__KEY'] = '0ebec1a366b4371a8567cff2fc3a2772'
 
 posts = [
     {
@@ -24,6 +27,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='about')
+
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', title='Register', form=form)
+	
+@app.route("/login")
+def register():
+	form = LoginForm()
+	return render_template('login.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
