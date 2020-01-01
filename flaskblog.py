30b492bee01a318449b6e961f28e991aff049993
Krichkorn Oparad <krichkorn@gmail.com>
2020-01-01 15:49:27 +0700

    Flash message of successful registration

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
-app.config['SECRET__KEY'] = '0ebec1a366b4371a8567cff2fc3a2772'
+# app.config['SECRET__KEY'] = '0ebec1a366b4371a8567cff2fc3a2772'
+
+import os
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 posts = [
     {
@@ -28,15 +32,21 @@ def home():
 def about():
     return render_template('about.html', title='about')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
 	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
 	return render_template('register.html', title='Register', form=form)
 	
 @app.route("/login")
 def login():
 	form = LoginForm()
-	return render_template('login.html', title='Register', form=form)
+	return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	#### normal computer
+    # app.run(debug=True)
+    #### ipad
+    app.run(use_reloader=False, debug=True)
